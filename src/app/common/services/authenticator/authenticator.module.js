e19a0929ae2589d4d6b6481344e35afdfdc6bc22
Cristiano de Souza Vinhal <cristiano.vinhal10@gmail.com>
2016-05-15 16:07:41 -0300

    resolved problems

--- src/app/common/services/authenticator/authenticator.module.js
+++
@@ -1,6 +0,0 @@
-import { Authenticator }  from './jsonWebToken.service';
-
-export default angular
-  .module('serviceOrder.services.JsonWebTokenService', [])
-  .service('authenticator', Authenticator)
-  .name;
--- src/app/common/services/json-web-token-service/json-web-token.module.js
+++
@@ -1,8 +0,0 @@
-import { JsonWebTokenService }  from './json-web-token.service';
-import { JsonWebTokenFactory }  from './json-web-token-factory';
-
-export default angular
-  .module('serviceOrder.services.json-web-token', [])
-  .service('JsonWebTokenService', JsonWebTokenService)
-  .factory('JsonWebTokenFactory', JsonWebTokenFactory)
-  .name;
